a1461a6926290faf57e95ba18ea27c9bbf1e86b4
YazanAK23 <abukhaledy02@gmail.com>
2023-10-01 00:09:32 +0300

    Edit the sign in

---
+++ MTIT-main/project-main/js/sign.js
@@ -0,0 +1,16 @@
+// function validateForm() {
+//     // Get the values of the username and password fields
+//     var email = document.getElementById("email").value;
+//     var password = document.getElementById("password").value;
+    
+//     // Check if the fields are empty
+//     if (username === "" || password === "") {
+//         alert("Both username and password are required.");
+//         return false; // Prevent form submission
+//     }
+// }
+// let SignIn = document.getElementById("SignIn");
+
+// console.log(SignIn);
+ 
+// SignIn.addEventListener("click",function(e){});
